7657b8904c58046598229186f4c1a4c82f52b038
pranayIntrigue <pranay@intrigueme.ca>
2020-03-31 14:48:24 -0400

    Added new fonts and created a business card layout

--- lib/main.dart
+++ lib/main.dart
@@ -16,28 +16,74 @@ class MyApp extends StatelessWidget {
           title: Text('IM Trivia'),
         ),
         body: SafeArea(
-            child: Row(
-          crossAxisAlignment:
-              CrossAxisAlignment.stretch, //or use width as infinity
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.red,
-              child: Text('Hello my name is Mark'),
-            ),
-            Center(
-              child: StackedWidgets(),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.blue,
-              child: Text('I am from New Foundland'),
-            ),
-          ],
-        )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/profile.jpeg'),
+              ),
+              Text(
+                'Pranay S',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans',
+                  fontSize: 25.0,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 75.0,
+                child: Divider(color: Colors.teal.shade200),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+555555555',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'admin@admin.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
